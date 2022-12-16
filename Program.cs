a301caa26ab73ffe21995f2647f7a4f55af51938
Алексей Поддубный <alkalineal54@gmail.com>
2022-12-16 20:14:41 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+bool CheckList(ref LinkedListNode<int>? first, ref LinkedListNode<int>? last)
+{
+
+    if (first != last || first?.Next != last?.Previous)
+    {
+        if (first?.Value == last?.Value)
+        {
+            var temp1 = first?.Next;
+            var temp2 = last?.Previous;
+            return CheckList(ref temp1, ref temp2);
+        }
+        return false;
+
+    }
+    else if (first?.Next == last?.Previous)
+    {
+        if (first?.Value == last?.Value)
+        {
+            return true;
+        }
+        return false;
+
+    }
+    else
+    {
+        return true;
+    }
+}
+
+var ints = new LinkedList<int>(new[] { 1, 2, 3, 4, 5, 6, 4, 3, 2, 1 });
+var temp1 = ints.First;
+var temp2 = ints.Last;
+Console.WriteLine(CheckList(ref temp1, ref temp2));
